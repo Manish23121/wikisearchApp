a84485415104d4580ef4981f95a54c28351b830a
Manish kumar sah <manishsah23121999@gmail.com>
2021-07-16 17:26:58 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.urlencoded());
+
+app.use(express.static('./js'));
+
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/',function(req,res){
+    return res.render('index');
+});
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
